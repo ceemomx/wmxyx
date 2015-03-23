c8997d67a5e6fe07453edb135449b8c3620f22d3
mengxi <iloveloony@gmail.com>
2015-03-24 01:11:06 +0800

    new index

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -22,8 +21,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+
+routes(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -31,6 +30,7 @@ app.use(function(req, res, next) {
     err.status = 404;
     next(err);
 });
+
 
 // error handlers
 
@@ -55,6 +55,7 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
+
 
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
+module.exports = function(app){
 
-module.exports = router;
+    app.get('/', function(req, res){
+        res.render('../views/index',{title:'nimei'});
+    })
+
+};
