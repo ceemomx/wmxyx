9255aa093dbf711520c82d2c8f6441d50bdb8994
mengxi <iloveloony@gmail.com>
2015-03-24 21:02:09 +0800

    add login

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 var indexPageRouter = require('../lib/main/router');
+var adminPageRouter = require('../lib/admin/router');
 
 module.exports = function(app){
 
     app.use('/',indexPageRouter);
-
+    app.use('/admin', adminPageRouter);
 };
