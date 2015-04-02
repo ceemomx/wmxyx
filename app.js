97c0db455509616a155d12ce2be482030b8f982f
mengxi <iloveloony@gmail.com>
2015-04-02 14:47:28 +0800

    fix upload

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var multer = require('multer');
-
+var ueditor = require("ueditor");
 
 var routes = require('./routes/index');
 
@@ -19,11 +19,32 @@ app.set('view engine', 'ejs');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: true}));
-app.use(multer({dest: './tmp'}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'upload')));
+app.use('/ueditor/file/action', ueditor(path.join(__dirname, '/public'), function(req, res, next) {
+    // ueditor 客户发起上传图片请求
+    if (req.query.action === 'uploadimage') {
+        var foo = req.ueditor;
+        var imgname = req.ueditor.filename;
+        var img_url = '/upload/images/';
+        //你只要输入要保存的地址 。保存操作交给ueditor来做
+        res.ue_up(img_url);
+    }
+    //  客户端发起图片列表请求
+    else if (req.query.action === 'listimage') {
+        var dir_url = '/upload/images/';
+        // 客户端会列出 dir_url 目录下的所有图片
+        res.ue_list(dir_url);
+    }
+    // 客户端发起其它请求
+    else {
+        res.setHeader('Content-Type', 'application/json');
+        res.redirect('/ueditor/nodejs/config.json');
+    }
+}));
+app.use(multer({dest: './tmp'}));
 
 
 routes(app);
